e487985f541fe2ca4ce131df9bbf9eaa42b7b155
gudurubabu <gudurubabu@users.noreply.github.com>
2018-05-23 06:48:39 +0530

    first commit
    first commit

---
+++ src/main/java/com/bujair/controller/MyAPIController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.bujair.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bujair
+ *
+ */
+@RestController
+public class MyAPIController {
+	
+
+}
